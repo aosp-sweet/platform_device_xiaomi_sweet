704c2616f9e84122a7b8c38b0c9a4bbf37e04671
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-25 17:40:28 +0600

    sweet: Import init.recovery.qcom.rc from LA.UM.9.1.r1-12000.01-SMxxx0.QSSI.12.0
    - And copy out to ramdisk target
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I78c8e91f573ca8512e96cd7e76f46e220fccfe07

--- device.mk
+++ device.mk
@@ -32,6 +32,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# Init scripts
+PRODUCT_PACKAGES += \
+    init.recovery.qcom.rc
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.xiaomi_sm6150
