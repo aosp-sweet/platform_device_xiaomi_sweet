44795edb63c77ca92f818ffa6bbaebc0846d2199
daniml3 <danimoral1001@gmail.com>
2021-09-02 05:23:36 +0000

    sweet: configs: Import stock display calibration config

--- device.mk
+++ device.mk
@@ -121,6 +121,10 @@ PRODUCT_PACKAGES += \
     libsdmcore-shim \
     disable_configstore
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml
+
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0.vendor \
