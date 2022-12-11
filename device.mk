3bdd159447ffb10fbe5c8436e2646393ba7cdc44
MoetaYuko <dianlujitao@lineageos.org>
2022-12-11 01:15:09 +0000

    sweet: Build libsensorndkbridge from source

--- device.mk
+++ device.mk
@@ -300,7 +300,8 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
