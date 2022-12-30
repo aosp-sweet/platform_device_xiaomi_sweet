eb7d96423381a43901751fac0c73d7c680cd3060
SebaUbuntu <barezzisebastiano@gmail.com>
2022-12-30 03:15:01 +0000

    sweet: rootdir: Switch to Android.bp
    * Cleanup duplicate qcom usb binary and init scripts
    * Keep prebuilts Ramdisk target with Android.mk
    
    Signed-off-by: email <aditya.kevin@hotmail.com>

--- device.mk
+++ device.mk
@@ -300,9 +300,7 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/init.qcom.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.usb.rc \
-    $(LOCAL_PATH)/rootdir/bin/init.mi.usb.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.mi.usb.sh \
-    $(LOCAL_PATH)/rootdir/bin/init.qcom.usb.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.usb.sh
+    $(LOCAL_PATH)/rootdir/bin/init.mi.usb.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.mi.usb.sh
 
 # Sensors
 PRODUCT_PACKAGES += \
